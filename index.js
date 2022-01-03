ca1401fc04e99276496def708f9cc847418c93dc
a1000rhan <1000rhan@gmail.com>
2022-01-03 11:35:24 +0300

    Aziz's Part

--- index.js
+++ index.js
@@ -8,35 +8,52 @@
  * ,... and so on
  */
 
-
 // PROGRAMMING
 // PrintConsole will be displaying an array that displays string
 // The butonClick will push elements into the array for display
-// if we put an operator, it does not print and will instead refresh for 
+// if we put an operator, it does not print and will instead refresh for
 // the next set of strings
 // AC will clear the array
 
-let numbersArray = []
-let assemblyArray = []
-let valueInput = valueInput + text
+let numbersArray = [];
+let assemblyArray = [];
+let valueInput = "";
 
 function buttonClick(text) {
   console.log("Clicking", text);
+  if (op(text)) {
+    printOnConsole(numbersArray);
+    for (let i = 0; i < numbersArray.length; i++) {
+      valueInput = valueInput + numbersArray[i];
+    }
+    assemblyArray.push(valueInput);
+    valueInput = "";
+    numbersArray = [];
+  } else {
+    if (text == 0 && numbersArray[0] == 0) {
+      numbersArray[0].pop();
+    } else if (text === "AC") {
+      numbersArray = [];
+    } else {
+      numbersArray.push(text);
+    }
+  }
 
-  if (text == 0 && numbersArray[0] == 0) { return 0}
-
-
-  else if (text != 0 && numbersArray[0]==0) {return 0}
-
-
-  else if (text === "AC") {return numbersArray = []} 
-
-
-  else {numbersArray.push(text)}
-  
   printOnConsole(numbersArray.join(""));
+
+  console.log(assemblyArray);
 }
 
+function op(text) {
+  const operator =
+    text == "+" ||
+    text == "-" ||
+    text == "x" ||
+    text == "÷" ||
+    text == "%" ||
+    text == "±";
+  return operator;
+}
 /** Supporting functions
  * 1. `printOnConsole(text)`, takes any text, and renders the console part of the web page
  * 2. `updateHistory(array)`, takes an array of strings and renders it on the web page
